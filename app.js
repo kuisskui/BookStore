c2ae5bf366516edea11ca3e33d9d771b99f32b5a
Kaopong Sangkittiyakorn <kuisskui@Kaopongs-MBP.true.true>
2023-04-09 14:26:13 +0700

    insert done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { columns } = require("mssql")
 const app = express()
 const databasePool = require("./database/mssql")
 const port = 8000
@@ -14,13 +15,6 @@ const soldBooks = require("./routers/soldBooks")
 
 app.set("view engine", "ejs")
 
-// app.use("/authors", authors)
-// app.use("/bookCategories", bookCategories)
-// app.use("/books", books)
-// app.use("/borrowers", borrowers)
-// app.use("/categories", categories)
-// app.use("/rentedBooks", rentedBooks)
-// app.use("/soldBooks", soldBooks)
 app.get('/', function (req, res) {
     databasePool.request()
         .query(`
@@ -38,47 +32,79 @@ app.get('/', function (req, res) {
         });
 })
 
-app.get('/table', async function (req, res) {
-    // databasePool.request()
-    //     .query(`SELECT * FROM ${req.params.table}`)
-    //     .then((result) => {
-    //         // console.log(result) // debug here
-    //         res.render(`${req.params.table}`, { data: result.recordset, "tableName": `${req.params.table}` })// point to template here
-    //     })
-    //     .catch((err) => {
-    //         console.error(`Error querying database: ${err}`);
-    //         res.sendStatus(500);
-    //     });
+app.get("/table", async function (req, res) {
     const tableName = req.query.tableName
-    const queryResult = await databasePool.request().query(`SELECT * FROM ${tableName}`);
-    const columnName = await databasePool.request().query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${tableName}'`);
+    const queryResult = await (await databasePool.request().query(`SELECT * FROM ${tableName}`))
+    const columnName = await (await databasePool.request().query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${tableName}'`))
 
     Promise.all([queryResult, columnName])
         .then((results) => {
             const result = results[0].recordset.map(object => Object.values(object))
             const columnName = results[1].recordset.map(object => Object.values(object))
-            res.render("table", {"tableName": tableName, "columnName": columnName, "result": result});
+            res.render("table", { "tableName": tableName, "columnName": columnName, "result": result });
         })
         .catch((err) => {
             console.error(`Error querying database: ${err}`);
             res.sendStatus(500);
         });
 })
+function createDataTypeObject(dataType) {
+    dataType = dataType.map(object => Object.values(object))
+    dataTypeObject = {}
+    for (const value of dataType) {
+        Object.defineProperty(dataTypeObject, value[0], {
+            value: value[1]
+        })
+    }
+    return dataTypeObject
+}
+function writeQueryString(dataType, query) {
+    dataTypeObject = createDataTypeObject(dataType)
+    let queryString = []
+    for (const p in query) {
+        const type = ["int", "decimal"]
+        const value = query[p]
+        if (type.includes(dataTypeObject[p])) {
+            queryString.push(`${value}`)
+        }
+        else {
+            queryString.push(`'${value}'`)
+        }
+    }
+    return queryString.join(',')
+}
 
-app.use("/insert", function (req, res, next) {
+app.get("/insert", async function (req, res) {
     const tableName = req.query.tableName
     const query = req.query
     delete query.tableName
-    const columnString = Object.keys(query).join(", ")
-    const valueString = Object.values(query).join(", ")     // fix if it is string quote them ''
-    console.log(valueString)
+
+    const dataType = databasePool.request()
+        .query(`
+        SELECT COLUMN_NAME, DATA_TYPE
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = '${tableName}';    
+    `)
+
+    let columnString;
+    let valueString;
+    await Promise.all([dataType])
+        .then((results) => {
+            columnString = Object.keys(query).join(", ")
+            valueString = writeQueryString(results[0].recordset, query)
+        })
+        .catch((err) => {
+            console.error(`Error querying database: ${err}`);
+            res.sendStatus(500);
+        });
+
     databasePool.request()
         .query(`
             INSERT INTO ${tableName} (${columnString})
             VALUES (${valueString})
         `)
         .then(() => {
-            next()
+            res.redirect(req.headers.referer)
         })
         .catch((err) => {
             console.error(`Error querying database: ${err}`);
@@ -87,10 +113,10 @@ app.use("/insert", function (req, res, next) {
 })
 
 app.get("/delete", function (req, res) {
-    
+
 })
 app.get("/update", function (req, res) {
-    
+
 })
 app.listen(port, function () {
     console.log(`Starting http://${server}:${port}`)
